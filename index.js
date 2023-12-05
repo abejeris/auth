31dffecaff336629cd45c7814b893ec1d81e224d
Audrius Bejeris <audriusbejeris@gmail.com>
2023-12-05 16:10:07 +0200

    adding mongoose f-ality

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 const User = require('./models/user');
 port = 3000;
+const mongoose = require('mongoose');
+
+mongoose
+	.connect('mongodb://127.0.0.1:27017/authDemo')
+	.then(() => {
+		console.log('Mongo connection open!');
+	})
+	.catch((err) => {
+		console.log('its the error message, and it says:');
+		console.log(err);
+	});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/register', (req, res) => {
 	res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+	res.send(req.body);
 });
 
 app.get('/secret', (req, res) => {
