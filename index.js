e72d42fcc653ce23eb146581f9f137ebcb375b40
Audrius Bejeris <audriusbejeris@gmail.com>
2023-12-05 19:35:14 +0200

    added middleware for auth to be used in any route

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'not a good secret' }));
 
+const requireLogin = (req, res, next) => {
+	if (!req.session.user_id) {
+		return res.redirect('/login');
+	}
+	next();
+};
+
 app.get('/', (req, res) => {
 	res.send('this is the home page');
 });
@@ -63,12 +70,14 @@ app.post('/logout', (req, res) => {
 	res.redirect('/login');
 });
 
-app.get('/secret', (req, res) => {
-	if (!req.session.user_id) {
-		return res.redirect('/login');
-	}
+app.get('/secret', requireLogin, (req, res) => {
 	res.render('secret');
 });
+
+app.get('/topsecret', requireLogin, (req, res) => {
+	res.send('top secret');
+});
+
 app.listen(port, () => {
 	console.log(`app running on port ${port}`);
 });
